f30180e1858af6dab4f3c251a991b793eedbe35f
Hafskjold03 <christoffer.hafskjold@gmail.com>
2026-02-28 10:21:13 +0100

    Added logout functionality

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import { LogoutButton } from "@/components/auth/logoutbutton";
+
 export default function DashboardPage() {
   return (
     <div className="p-8">
       <h1 className="text-2xl font-bold">Dashboard</h1>
       <p className="mt-2">You are logged in.</p>
+
+      <LogoutButton />
     </div>
   );
 }
